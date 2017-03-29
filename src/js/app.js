35ec9fc054ee696c5987b15c47442e8dcdc19edf
constsync <timothy.mbc@gmail.com>
2017-03-29 01:42:23 -0400

    General Refactoring

--- src/js/app.js
+++ src/js/app.js
@@ -3,43 +3,43 @@ angular.module('colland', [])
     .controller('mainCtrl', mainCtrl)
 
     function mainCtrl ($scope) {
-        const stockQuestions = [
+        $scope.stockQuestions = [
             {
                 "Name": "What is you client's federal bracket?",
                 "Type": "Long answer",
-                "plugins": ["RightBridge", "FireLight"],
-                "timestamp": "2012-04-23T18:25:43.511Z"
+                "Plugins": ["RightBridge", "FireLight"],
+                "TimeStamp": "2012-04-23T18:25:43.511Z"
             },
 
             {
                 "Name": "What is you client's risk tolerance?",
                 "Type": "Multiple choice",
-                "plugins": ["RightBridge", "FireLight"],
-                "timestamp": "2012-04-23T18:25:43.511Z"
+                "Plugins": ["RightBridge", "FireLight"],
+                "TimeStamp": "2012-04-23T18:25:43.511Z"
             },
 
             {
                 "Name": "What is your martial status?",
                 "Type": "Multiple choice",
-                "plugins": ["RightBridge"],
-                "timestamp": "2012-04-23T18:25:43.511Z"
+                "Plugins": ["RightBridge"],
+                "TimeStamp": "2012-04-23T18:25:43.511Z"
             },
 
             {
                 "Name": "What are your financial objectives?",
                 "Type": "Multiple select",
-                "plugins": ["FireLight"],
-                "timestamp": "2012-04-23T18:25:43.511Z"
+                "Plugins": ["FireLight"],
+                "TimeStamp": "2012-04-23T18:25:43.511Z"
             },
 
             {
                 "Name": "How long do you intend to keep this purchase?",
                 "Type": "Short Answer",
-                "plugins": ["RightBridge", "FireLight"],
-                "timestamp": "2012-04-23T18:25:43.511Z"
+                "Plugins": ["RightBridge", "FireLight"],
+                "TimeStamp": "2012-04-23T18:25:43.511Z"
             }
 
         ];
 
-        console.debug('stockQuestions: ', stockQuestions);
+        console.debug('stockQuestions: ', $scope.stockQuestions);
     }
